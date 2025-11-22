f1dbae67f7e8e213d7a65afc8c1963e405eb3dec
Amirouche A BOUBEKKI <hello@amirouche.dev>
2025-11-22 11:37:24 +0100

    That is demo file, not an example file, the import form is invalid

--- examples/example_with_ouverture.py
+++
@@ -1,7 +0,0 @@
-from mobius.pool import c0ffeebad as kawa
-
-def combine_results(data):
-    """Combine results using kawa function."""
-    processed = kawa(data)
-    final = processed * 2
-    return final
